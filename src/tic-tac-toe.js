c71b38276d8929126cc84b312a646fe19e1337fb
yariktobolich <yarik.tobolich@gmail.com>
2017-01-31 02:22:32 +0300

    tic-tac-toe:init commit

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,33 @@
 class TicTacToe {
     constructor() {
-
+        this.current = "x";
+        this.arr = [[null,null,null],[null,null,null],[null,null,null]];
+        this.cell = 9;
+        this.win = null;
     }
-
     getCurrentPlayerSymbol() {
-
+        return this.current;
     }
-
     nextTurn(rowIndex, columnIndex) {
-
+        if(this.arr[rowIndex][columnIndex] === null) {
+        this.arr[rowIndex][columnIndex] = this.current;
+        if(this.current === "x"){
+            return this.current = "o";
+        } else {
+            return this.current = "x"
+        }
+        this.cell--;
+        }
     }
-
     isFinished() {
-
     }
-
     getWinner() {
-
     }
-
     noMoreTurns() {
-
     }
-
     isDraw() {
-
     }
-
     getFieldValue(rowIndex, colIndex) {
-
     }
 }
-
 module.exports = TicTacToe;
